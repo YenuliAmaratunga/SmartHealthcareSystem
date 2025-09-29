d4b2f72e9374bb10b01c7e17c052f3d1df41206f
YenuliAmaratunga <yenulibeemansi@gmail.com>
2025-09-29 17:49:01 +0530

    Initial folder structure

---
+++ frontend/hospital360/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Hello Hospital360 👋
+      </h1>
+    </div>
+  );
+}
