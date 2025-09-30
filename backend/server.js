9d5e7e916a582633433576b9c92ba09b13042d63
SachiniGayathma <sachinigayathma@yahoo.com>
2025-09-30 20:45:15 +0530

    Server.js file initialized

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+const PORT = 8080
+const app = express();
+app.use(cors());
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log('Connected to MongoDB');
+})
+.catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+});
+
+
+app.listen(PORT,()=>{
+
+    console.log(`App Listens To The Port ${PORT}`);
+});
+
