14d7847632ad498f5b080d3c5d1dc31fdda417e7
Dhiyanah <dheeliyaudeen@gmail.com>
2025-10-16 15:26:51 +0530

    App.jsx home rendering

--- frontend/hospital360/src/App.jsx
+++ frontend/hospital360/src/App.jsx
@@ -1,15 +1,3 @@
-/*import React from "react";
-import CheckInDashboard from "./pages/PatientCheckInDashboard"; 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-export default function App() {
-  return (
-    <div className="min-h-screen">
-      <CheckInDashboard />
-    </div>
-  );
-} */
-
 import { Routes, Route } from "react-router-dom";
 import CheckInDashboard from "./pages/PatientCheckInDashboard";
 import PatientDetailsPage from "./pages/PatientDetailsPage";
@@ -21,10 +9,6 @@ export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/patients" element={<CheckInDashboard />} />
-      <Route path="/patients/:id" element={<PatientDetailsPage />} />
-      <Route path="/patients/register-patient" element={<RegisterPatientFormPage />} />
-      <Route path="/patients/register-temp-patient" element={<TemporaryPatientFormPage />} />
     </Routes>
   );
 }
