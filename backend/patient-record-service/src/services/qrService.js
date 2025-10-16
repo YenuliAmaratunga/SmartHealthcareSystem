bbbf98db282dfaca3a75e9bceb2f3f4963f31fc6
SachiniGayathma <sachinigayathma@yahoo.com>
2025-10-17 00:18:08 +0530

    g

--- backend/patient-record-service/src/services/qrService.js
+++
@@ -1,6 +0,0 @@
-import QRCode from "qrcode";
-
-export const generateQRCode = async (patientId) => {
-  const qrData = { patientId };
-  return await QRCode.toDataURL(JSON.stringify(qrData));
-};
--- frontend/hospital360/src/App.jsx
+++ frontend/hospital360/src/App.jsx
@@ -4,11 +4,6 @@ import AvailabilityResults from "./pages/AvailabilityResults";
 import BookAppointment from "./pages/BookAppointment";
 
 
-import CheckInDashboard from "./pages/PatientCheckInDashboard";
-import PatientDetailsPage from "./pages/PatientDetailsPage";
-import RegisterPatientFormPage from "./pages/RegisterPatientFormPage";
-import TemporaryPatientFormPage from "./pages/TemporaryPatientFormPage";
-
 
 export default function App() {
   return (
