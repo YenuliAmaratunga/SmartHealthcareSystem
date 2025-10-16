858b3f69de9e259da864c349262ae39750c38a5c
SachiniGayathma <sachinigayathma@yahoo.com>
2025-10-17 02:51:34 +0530

    app.jsx error fixed

--- frontend/hospital360/src/App.jsx
+++ frontend/hospital360/src/App.jsx
@@ -3,12 +3,19 @@ import CheckInDashboard from "./pages/PatientCheckInDashboard";
 import PatientDetailsPage from "./pages/PatientDetailsPage";
 import RegisterPatientFormPage from "./pages/RegisterPatientFormPage";
 import TemporaryPatientFormPage from "./pages/TemporaryPatientFormPage";
-import Home from "./pages/Home";
+import SchAppointments from "./pages/SchAppointments";
+import AvailabilityResults from "./pages/AvailabilityResults";
+import BookAppointment from "./pages/BookAppointment";
+
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+   
+      <Route path = "/scheduleAppointments" element = {<SchAppointments/>}/>
+      <Route path="/availability" element = {<AvailabilityResults/>}/>
+      <Route path = "/book-appointment" element = {<BookAppointment/>}/>
+   
     </Routes>
   );
 }
