dac60ee3eaa3320c9bd03637e26f7877451f33e5
CORPNET\dhpilk <dhanushi.piyaratne@ifs.com>
2025-10-17 09:15:06 +0530

    final changes payment system

--- frontend/hospital360/src/App.jsx
+++ frontend/hospital360/src/App.jsx
@@ -1,14 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-<<<<<<< HEAD
-import SchAppointments from "./pages/SchAppointments";
-import AvailabilityResults from "./pages/AvailabilityResults";
 
-=======
->>>>>>> origin/develop
+// Patient Check-In Routes
 import CheckInDashboard from "./pages/PatientCheckInDashboard";
 import PatientDetailsPage from "./pages/PatientDetailsPage";
 import RegisterPatientFormPage from "./pages/RegisterPatientFormPage";
 import TemporaryPatientFormPage from "./pages/TemporaryPatientFormPage";
+
+// Appointment Routes
 import SchAppointments from "./pages/SchAppointments";
 import AvailabilityResults from "./pages/AvailabilityResults";
 import BookAppointment from "./pages/BookAppointment";
@@ -22,29 +20,22 @@ import PaymentFailurePage from "./pages/PaymentFailurePage";
 export default function App() {
   return (
     <Routes>
-<<<<<<< HEAD
-      {/* Appointment Routes */}
-      <Route path="/scheduleAppointments" element={<SchAppointments />} />
-      <Route path="/availability" element={<AvailabilityResults />} />
-
       {/* Patient Check-In Routes */}
       <Route path="/check-in" element={<CheckInDashboard />} />
       <Route path="/patient-details/:id" element={<PatientDetailsPage />} />
       <Route path="/register-patient" element={<RegisterPatientFormPage />} />
       <Route path="/temporary-patient" element={<TemporaryPatientFormPage />} />
 
+      {/* Appointment Routes */}
+      <Route path="/scheduleAppointments" element={<SchAppointments />} />
+      <Route path="/availability" element={<AvailabilityResults />} />
+      <Route path="/book-appointment" element={<BookAppointment />} />
+
       {/* Payment Routes */}
       <Route path="/bills-and-payments" element={<BillsAndPaymentsPage />} />
       <Route path="/payment" element={<PaymentPage />} />
       <Route path="/payment-success" element={<PaymentSuccessPage />} />
       <Route path="/payment-failure" element={<PaymentFailurePage />} />
-=======
-   
-      <Route path = "/scheduleAppointments" element = {<SchAppointments/>}/>
-      <Route path="/availability" element = {<AvailabilityResults/>}/>
-      <Route path = "/book-appointment" element = {<BookAppointment/>}/>
-   
->>>>>>> origin/develop
     </Routes>
   );
 }
